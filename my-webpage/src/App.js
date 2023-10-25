13d26d4633096070e93433c470b0ef76ff47231a
IamMatt117 <81575818+IamMatt117@users.noreply.github.com>
2023-10-25 19:54:46 +0100

    added images

--- my-webpage/src/App.js
+++ my-webpage/src/App.js
@@ -7,6 +7,9 @@ import emailRed from './images/emailRed.png'
 import gitlab from './images/gitlab.png'
 import gitlabHover from './images/gitlabHover.png'
 import resume from './images/Resume-cropped.svg'
+import haul from './images/BigHaul.png'
+import park from './images/park.jpg'
+import road from './images/car.png'
 
 
 function App() {
@@ -40,7 +43,7 @@ function App() {
           <div>
           <div className='column-left' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>The Big Haul</h3>
-
+            <img style={{width: '250px', margin: 'auto', padding: 'auto', display: 'block'}} src={haul}></img>
           </div>
           <div className='column-right' style={{background: '#F1F0F0', position: 'relative'}}>
             <p style={{marginLeft: '10px'}}>Users are asked a series of questions that they have to answer.The first answer will earn the user $100. Correct answers from that point forward will double what's in the users bank. Each incorrect answer will remove a life. Failing the final round will result in a game over. The aim is to reach $500,000 with at least 1 life. You reach $500,000 if you earn the Big Haul by reaching the end. Losing 3 lives will cause the User to lose and the game ends. Each game holds a variety of rounds with each round being unique while keeping them dynamic to make sure not every game will play out the same as the previous one.</p>
@@ -48,6 +51,7 @@ function App() {
             </div>
           <div className='column-right' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>Park at DCU</h3>
+            <img src={park} style={{width: '250px', margin: 'auto', padding: 'auto', display: 'block'}}></img>
             </div>
             <div className='column-left' style={{background: '#F1F0F0', position: 'relative'}}>
             <p>Struggling to park at DCU? This Web App retrieves available spaces from all DCU carparks and relays that information back to the user. Simply input the carpark you wish to use and the Web App will display the number of available spaces (if any).</p>
@@ -62,6 +66,7 @@ function App() {
           </div>
           <div className='column-right' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>Road Ready - In Progress</h3>
+            <img src={road} style={{height: '238px', margin: 'auto', padding: 'auto', display: 'block'}}></img>
             </div>
             <div className='column-left' style={{background: '#F1F0F0', position: 'relative'}}>
             <p>A Mobile App designed to create a one stop place to study for the Driving/Theory Test. The app hosts 3 main features. The first feature being Learning Material, a section containing any information a user might need about rules of the road. The second feature is a Theory Questionnaire with feedback, this feature is designed to allow the user to practice their knowledge for the Theory Test with feedback to help them improve on areas where they are weak. The last feature is a a tool with prebuilt test routes and ability to create new routes.</p>
