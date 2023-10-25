2fd427e34768405b7c1663a73995192d7080e6a8
IamMatt117 <81575818+IamMatt117@users.noreply.github.com>
2023-10-25 21:18:33 +0100

    grid styling

--- my-webpage/src/App.js
+++ my-webpage/src/App.js
@@ -10,6 +10,7 @@ import resume from './images/Resume-cropped.svg'
 import haul from './images/BigHaul.png'
 import park from './images/park.jpg'
 import road from './images/car.png'
+import django from './images/Django.png'
 
 
 function App() {
@@ -41,36 +42,39 @@ function App() {
         </div>
           <h3 id='Projects' style={{textAlign: 'center', color: '#015761'}}>Projects</h3>
           <div>
-          <div className='column-left' style={{background: '#015761', color: '#F7DC4F'}}>
+          <div className="container">
+          <div className='item-a' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>The Big Haul</h3>
             <img style={{width: '250px', margin: 'auto', padding: 'auto', display: 'block'}} src={haul}></img>
           </div>
-          <div className='column-right' style={{background: '#F1F0F0', position: 'relative'}}>
+          <div className='item-a-p' style={{background: '#F1F0F0', position: 'relative'}}>
             <p style={{marginLeft: '10px'}}>Users are asked a series of questions that they have to answer.The first answer will earn the user $100. Correct answers from that point forward will double what's in the users bank. Each incorrect answer will remove a life. Failing the final round will result in a game over. The aim is to reach $500,000 with at least 1 life. You reach $500,000 if you earn the Big Haul by reaching the end. Losing 3 lives will cause the User to lose and the game ends. Each game holds a variety of rounds with each round being unique while keeping them dynamic to make sure not every game will play out the same as the previous one.</p>
             <a className='code-button' href="https://gitlab.com/group7-big-haul/The_Big_Haul" style={{marginLeft: '10px', padding: '5px', bottom: '10px', position: 'absolute'}}>Veiw Code</a>
             </div>
-          <div className='column-right' style={{background: '#015761', color: '#F7DC4F'}}>
+          <div className='item-b' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>Park at DCU</h3>
             <img src={park} style={{width: '250px', margin: 'auto', padding: 'auto', display: 'block'}}></img>
             </div>
-            <div className='column-left' style={{background: '#F1F0F0', position: 'relative'}}>
+            <div className='item-b-p' style={{background: '#F1F0F0', position: 'relative'}}>
             <p>Struggling to park at DCU? This Web App retrieves available spaces from all DCU carparks and relays that information back to the user. Simply input the carpark you wish to use and the Web App will display the number of available spaces (if any).</p>
             <a className='code-button' href="https://gitlab.computing.dcu.ie/mahonm28/2023-ca377-mahonm28-parkatdcu" style={{marginLeft: '10px', padding: '5px', bottom: '10px', position: 'absolute'}}>Veiw Code</a>
           </div>
-          <div className='column-left' style={{background: '#015761', color: '#F7DC4F'}}>
+          <div className='item-c' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>Django Web App - In Progress</h3>
+            <img src={django}style={{width: '250px', margin: 'auto', padding: 'auto', display: 'block'}}></img>
             </div>
-            <div className='column-right' style={{background: '#F1F0F0', position: 'relative'}}>
+            <div className='item-c-p' style={{background: '#F1F0F0', position: 'relative'}}>
             <p>This is a simple Web App designed using Django.</p>
             <a className='code-button' href="https://gitlab.computing.dcu.ie/rawata2/2024-ca4094-rawata2-mahonm28" style={{marginLeft: '10px', padding: '5px', bottom: '10px', position: 'absolute'}}>Veiw Code</a>
           </div>
-          <div className='column-right' style={{background: '#015761', color: '#F7DC4F'}}>
+          <div className='item-d' style={{background: '#015761', color: '#F7DC4F'}}>
             <h3>Road Ready - In Progress</h3>
             <img src={road} style={{height: '238px', margin: 'auto', padding: 'auto', display: 'block'}}></img>
             </div>
-            <div className='column-left' style={{background: '#F1F0F0', position: 'relative'}}>
+            <div className='item-d-p' style={{background: '#F1F0F0', position: 'relative'}}>
             <p>A Mobile App designed to create a one stop place to study for the Driving/Theory Test. The app hosts 3 main features. The first feature being Learning Material, a section containing any information a user might need about rules of the road. The second feature is a Theory Questionnaire with feedback, this feature is designed to allow the user to practice their knowledge for the Theory Test with feedback to help them improve on areas where they are weak. The last feature is a a tool with prebuilt test routes and ability to create new routes.</p>
             <a className='code-button' href="https://gitlab.computing.dcu.ie/mahonm28/2024-ca472-mahonm28-rawata2" style={{marginLeft: '10px', padding: '5px', bottom: '10px', position: 'absolute'}}>Veiw Code</a>
+          </div>
           </div>
             <h3 style={{color: '#015761'}}>Resume</h3>
           <div id='Resume' style={{background: '#F1F0F0'}}>
